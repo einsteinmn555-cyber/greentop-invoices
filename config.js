65bd095e9aff5b031250d95dbd94873675273f24
einsteinmn555-cyber <einsteinmn555@gmail.com>
2026-07-24 20:50:55 +0300

    إضافة رابط وظيفة الحجز وتحديث بريد الإدارة

--- config.js
+++ config.js
@@ -2,8 +2,9 @@ window.GREENTOP_CONFIG = Object.freeze({
   SUPABASE_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_tAQBUzPNyyAImnHAMmL0aQ_fnrrjNQp',
   INVOICE_FUNCTION_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co/functions/v1/get-invoice-url',
+  BOOKING_FUNCTION_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co/functions/v1/booking-notification',
   CUSTOMER_PORTAL_ORIGIN: 'https://greentop-invoices.pages.dev',
   OFFICIAL_WEBSITE: 'https://www.greentaxikw.com',
-  ADMIN_EMAIL: 'info@greentaxikw.com',
+  ADMIN_EMAIL: 'taxigreentop@gmail.com',
   MAX_FILE_SIZE_MB: 10,
 });
