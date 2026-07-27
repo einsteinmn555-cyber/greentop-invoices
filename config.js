4ee79b1d6567b53089f42a7703f8d071659fa049
einsteinmn555-cyber <einsteinmn555@gmail.com>
2026-07-27 16:54:02 +0300

    فصل منصة الحجز عن بوابة الفواتير

--- config.js
+++ config.js
@@ -2,9 +2,8 @@ window.GREENTOP_CONFIG = Object.freeze({
   SUPABASE_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_tAQBUzPNyyAImnHAMmL0aQ_fnrrjNQp',
   INVOICE_FUNCTION_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co/functions/v1/get-invoice-url',
-  BOOKING_FUNCTION_URL: 'https://mhyzbobdojfqyjbvgxow.supabase.co/functions/v1/submit-booking-test',
   CUSTOMER_PORTAL_ORIGIN: 'https://greentop-invoices.pages.dev',
   OFFICIAL_WEBSITE: 'https://www.greentaxikw.com',
-  ADMIN_EMAIL: 'taxigreentop@gmail.com',
+  ADMIN_EMAIL: 'info@greentaxikw.com',
   MAX_FILE_SIZE_MB: 10,
 });
